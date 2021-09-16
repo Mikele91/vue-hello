2ec1f41f179581ed78676f09349ac1b4ea8619c3
Mikele91 <michele.amoruso91.8@gmail.com>
2021-09-16 16:26:08 +0200

    fine

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 var app = new Vue({
     el:"#root",
     data:{
-        message:"ciao a tutti",
-        myColor:["color1", "color2"],
-        color:"color1"
+        message:"Cosa sarà mai ???",
+        message2:"Benvenuto Vue Js",
+        color:"color1",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png",
+        visibile:false
     },
-    methods:{
-        this.color = (this.color== "color1")? "color2":"color1"
+    methods: {
+        changeColor :function(){
+        this.color = (this.color== "color1")? "color2":"color1",
+        this.visibile= (this.visibile == false)? true: false
+        }
     }
 }
 
